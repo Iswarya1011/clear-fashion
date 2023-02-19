0fa8b0ad910dfbac371d82c111fff7b6c3f56e3a
Iswarya Sivasubramaniam <iswarya.sivasubramaniam@edu.devinci.fr>
2023-02-19 15:52:22 +0100

    Update sandbox.js

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-console, no-process-exit */
+/** 
 const dedicatedbrand = require('./eshops/dedicatedbrand');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/all-men') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
 
@@ -17,6 +18,48 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
 }
 
 const [,, eshop] = process.argv;
+*/
 
-//sandbox(eshop);
-sandbox(sandbox(eshop));
+/** 
+const circlesport = require('./eshops/circlesport');
+
+async function sandbox (eshop = 'https://shop.circlesportswear.com/collections/collection-femme') {
+  try {
+    console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
+
+    const products = await circlesport.scrape(eshop);
+
+    console.log(products);
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+const [,, eshop] = process.argv;
+*/
+
+
+
+
+const montlimart = require('./eshops/montlimart');
+
+async function sandbox (eshop = 'https://www.montlimart.com/99-vetements') {
+  try {
+    console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
+
+    const products = await montlimart.scrape(eshop);
+
+    console.log(products);
+    console.log('done');
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+const [,, eshop] = process.argv;
+
+sandbox(eshop);
+
