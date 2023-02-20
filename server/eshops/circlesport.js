ab9f0b3288716311109dce383267a295f98c67ab
Iswarya Sivasubramaniam <iswarya.sivasubramaniam@edu.devinci.fr>
2023-02-20 21:04:24 +0100

    Update circlesport.js

--- server/eshops/circlesport.js
+++ server/eshops/circlesport.js
@@ -27,12 +27,14 @@ const parse = data => {
        .replace(/\s/g, ' ');
 
        caracteristic=caracteristic.substr(0, caracteristic.length/2);
+       console.log("interieur")
       const price = parseInt(
         $(element)
           .find('.price__regular .money')
           .text().slice(1)
       
       );
+      price=5;
       
 
       return {name,caracteristic, price};
