f4ce38a7072f1a999323c6a8d6d84fe48afa1d9d
Iswarya Sivasubramaniam <iswarya.sivasubramaniam@edu.devinci.fr>
2023-02-20 21:04:29 +0100

    Update sandbox.js

--- server/sandbox.js
+++ server/sandbox.js
@@ -50,7 +50,7 @@ const [,, eshop] = process.argv;
 
 const montlimart = require('./eshops/montlimart');
 
-async function sandbox (eshop = 'https://www.montlimart.com/72-nouveautes') {
+async function sandbox (eshop = 'https://www.montlimart.com/99-vetements') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
 
@@ -59,12 +59,15 @@ async function sandbox (eshop = 'https://www.montlimart.com/72-nouveautes') {
     console.log(products);
     console.log('done');
     process.exit(0);
+    
   } catch (e) {
     console.error(e);
     process.exit(1);
+    
   }
 }
 const [,, eshop] = process.argv;
+
 sandbox(eshop);
 
 
