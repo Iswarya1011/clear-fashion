601c81b9734eef237fae0ae91049329bb70bde4f
Iswarya Sivasubramaniam <iswarya.sivasubramaniam@edu.devinci.fr>
2023-03-21 16:31:47 +0100

    Update api.js

--- server/api.js
+++ server/api.js
@@ -30,7 +30,7 @@ app.get('/', async(request, response) => {
   const client = await MongoClient.connect(MONGODB_URI, { 'useNewUrlParser': true });
   const db = client.db(MONGODB_DB_NAME);
   const collection = db.collection('products');
-  const products = await collection.find().array();
+  const products = await collection.find();
   response.send(products);
   
 
